fe528c0d5cbda73ed0da386534439fd56a04883e
SivaSankarMutte <84972317+SivaSankarMutte@users.noreply.github.com>
2021-09-21 21:06:42 +0530

    Update settings.py

--- ExamPortal/settings.py
+++ ExamPortal/settings.py
@@ -139,7 +139,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='sivasankarmutte@gmail.com'
-EMAIL_HOST_PASSWORD="K@lmno@123"
+EMAIL_HOST_PASSWORD=""
 
 
 MESSAGE_TAGS = {
